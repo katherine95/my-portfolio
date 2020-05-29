7c16e96b5c7cd7f9028eaeee29e051a044a9e046
katherine95 <kathiekim95@gmail.com>
2020-05-29 22:30:00 +0300

    Updates

---
+++ precache-manifest.9c6bf7c1bfa340095374b6c6abe4d63f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a866e8998693d667496e1c4b83234c",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "e65822347e93354b81c7",
+    "url": "/my-portfolio/static/css/main.fcbaf858.chunk.css"
+  },
+  {
+    "revision": "2da85ab95c96288e41dd",
+    "url": "/my-portfolio/static/js/2.00c99d44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.00c99d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e65822347e93354b81c7",
+    "url": "/my-portfolio/static/js/main.7e78da2b.chunk.js"
+  },
+  {
+    "revision": "d4a6f897414192edb936",
+    "url": "/my-portfolio/static/js/runtime-main.7e7d7162.js"
+  }
+]);
