2796ddc8ee47d7e65b1d88a373ee31542ae28e37
katherine95 <kathiekim95@gmail.com>
2020-05-29 13:03:24 +0300

    initial set up

---
+++ src/Components/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Portforlio = () => {
+  return (
+    <p>
+        Start of something = new type(arguments);
+    </p>
+  );
+}
+
+export default Portforlio;
